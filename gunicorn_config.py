d00e78386a8bf683253736cff91e8cc391354f04
Zhensu Sun <zhensuuu@gmail.com>
2025-06-13 22:33:34 +0800

    now it can run locally

--- gunicorn_config.py
+++ gunicorn_config.py
@@ -1,13 +1,13 @@
 import multiprocessing
 
 # 绑定的地址和端口
-bind = "127.0.0.1:8000"
+bind = "127.0.0.1:8082"
 
 # 工作进程数
-workers = multiprocessing.cpu_count() * 2 + 1
+workers = 1  # 减少worker数量以便调试
 
 # 工作模式
-worker_class = "eventlet"
+worker_class = "sync"  # 使用默认的sync worker
 
 # 超时时间
 timeout = 300
@@ -17,10 +17,10 @@ accesslog = "logs/access.log"
 errorlog = "logs/error.log"
 
 # 日志级别
-loglevel = "info"
+loglevel = "debug"  # 改为debug级别以获取更多信息
 
 # 守护进程模式
-daemon = True
+daemon = True  # 改为后台运行
 
 # 进程名称
 proc_name = "paper_podcast" 
